a8cfb29cc342083299c16a6ab383bfeab4f8ea8d
Christian Jensen <38145685+Zubat-Horde@users.noreply.github.com>
2019-08-28 19:05:20 -0500

    found bug and fixed bug

--- exercise-tracker/mern-exercise-tracker/backend/routes/exercises.js
+++ exercise-tracker/mern-exercise-tracker/backend/routes/exercises.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-let User = require('../models/exercise.model');
+let Exercise = require('../models/exercise.model');
 
 router.route('/').get((req, res) => {
-    User.find()
+        Exercise.find()
         .then(users => res.json(users))
         .catch(err => res.status(400).json('Error: ' + err));
 });
